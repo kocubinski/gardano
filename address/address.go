fa3ea7b1fed4ea192f0046f8175ed5f8ff55292a
Matt Kocubinski <mkocubinski@gmail.com>
2025-02-25 15:04:49 -0600

    basic address encoding

--- address/address.go
+++ address/address.go
@@ -1,3 +1,39 @@
 package address
 
+import (
+	"fmt"
+
+	"github.com/gcash/bchutil/bech32"
+	"golang.org/x/crypto/blake2b"
+)
+
+const Blake2b224Len = 28
+
 type Address string
+
+func Blake2b224(data []byte) (result [Blake2b224Len]byte, err error) {
+	b2b, err := blake2b.New(Blake2b224Len, nil)
+	if err != nil {
+		err = fmt.Errorf("error blake2b224 init: %w", err)
+		return
+	}
+	b2b.Write(data)
+	copy(result[:], b2b.Sum(nil)[:Blake2b224Len])
+	return
+}
+
+func NewMainnetPaymentOnly(pub []byte) (Address, error) {
+	header := byte(0b01100001)
+	addr := []byte{header}
+	keyHash, err := Blake2b224(pub)
+	if err != nil {
+		return "", err
+	}
+	addr = append(addr, keyHash[:]...)
+	addr5Bit, err := bech32.ConvertBits(addr, 8, 5, true)
+	if err != nil {
+		return "", err
+	}
+	res, err := bech32.Encode("addr", addr5Bit)
+	return Address(res), err
+}
--- main_test.go
+++ main_test.go
@@ -7,18 +7,20 @@ import (
 
 	"github.com/blinklabs-io/gouroboros/cbor"
 	"github.com/gcash/bchutil/bech32"
+	"github.com/kocubinski/go-cardano/address"
 	"github.com/stretchr/testify/require"
 )
 
 const (
-	sKey1Cbor   = "5820816ae1825fd44eec487b0d8b0de63eb85e1f15ea9fd71ffef6bdf0291197406d"
-	vKey1Cbor   = "5820291ad90a6a5369be7b78f08810f64c75c8070976a638baf0aabe3960b8acb773"
-	sKey2Bech32 = "addr_sk1p7m6rndxn0s097zn4ga628dtm6zakep8x62vfnlnmpj3v9feey4shy5xx7"
-	skey2Bech32 = "addr_vk1rmvagkuqm0zqjmrw3p8m6k6ejc38zj5x68lfgzln4vwurc0gmdnqqhy5ad"
+	skey1Cbor   = "5820816ae1825fd44eec487b0d8b0de63eb85e1f15ea9fd71ffef6bdf0291197406d"
+	vkey1Cbor   = "5820291ad90a6a5369be7b78f08810f64c75c8070976a638baf0aabe3960b8acb773"
+	skey2Bech32 = "addr_sk1p7m6rndxn0s097zn4ga628dtm6zakep8x62vfnlnmpj3v9feey4shy5xx7"
+	vkey2Bech32 = "addr_vk1rmvagkuqm0zqjmrw3p8m6k6ejc38zj5x68lfgzln4vwurc0gmdnqqhy5ad"
+	addrBech32  = "addr1vx64czye08j6pkysk3fyfm90jz2tyft5tzjktz5h22wkn2gqhw2sa"
 )
 
 func TestKeys_CBOR(t *testing.T) {
-	cborBz, err := hex.DecodeString(sKey1Cbor)
+	cborBz, err := hex.DecodeString(skey1Cbor)
 	require.NoError(t, err)
 	var bz []byte
 	_, err = cbor.Decode(cborBz, &bz)
@@ -29,12 +31,12 @@ func TestKeys_CBOR(t *testing.T) {
 	require.NoError(t, err)
 	pubKeyCbor, err := cbor.Encode(pub)
 	require.NoError(t, err)
-	require.Equal(t, sKey1Cbor, hex.EncodeToString(privKeyCbor))
-	require.Equal(t, vKey1Cbor, hex.EncodeToString(pubKeyCbor))
+	require.Equal(t, skey1Cbor, hex.EncodeToString(privKeyCbor))
+	require.Equal(t, vkey1Cbor, hex.EncodeToString(pubKeyCbor))
 }
 
 func TestKeys_Bech32(t *testing.T) {
-	hrp, data, err := bech32.Decode(sKey2Bech32)
+	hrp, data, err := bech32.Decode(skey2Bech32)
 	require.NoError(t, err)
 	require.Equal(t, "addr_sk", hrp)
 	skey, err := bech32.ConvertBits(data, 5, 8, false)
@@ -45,5 +47,8 @@ func TestKeys_Bech32(t *testing.T) {
 	require.NoError(t, err)
 	pubBech32, err := bech32.Encode("addr_vk", data)
 	require.NoError(t, err)
-	require.Equal(t, skey2Bech32, pubBech32)
+	require.Equal(t, vkey2Bech32, pubBech32)
+	addr, err := address.NewMainnetPaymentOnly(pub)
+	require.NoError(t, err)
+	require.Equal(t, addrBech32, string(addr))
 }
